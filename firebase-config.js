25bd282bc72f7adbbe92938bfe7bae48a15e70cd
Debby9393 <bakareharuna1@gmail.com>
2025-06-25 22:11:02 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUX52RwxuvOOx...",
+  apiKey: "AIzaSyDUX52RwxuvOOxW9Yb7p8B9WOhnz-OxOjo",
   authDomain: "tosin-appsheet.firebaseapp.com",
   projectId: "tosin-appsheet",
-  storageBucket: "tosin-appsheet.appspot.com",
+  storageBucket: "tosin-appsheet.firebasestorage.app",
   messagingSenderId: "380285961021",
-  appId: "1:380285961021:web:xxxxxxxxxxxxxxxx"
+  appId: "1:380285961021:web:c2b65cddeaf8904ac83b54",
+  measurementId: "G-91DPPCFB4C"
 };
 firebase.initializeApp(firebaseConfig);
